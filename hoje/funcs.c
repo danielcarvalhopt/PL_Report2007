c89435c801c81863d753016a5ea60601821b6ea8
Serafim Pinto <smcp92@gmail.com>
2013-06-02 20:09:01 +0100

    ...

--- hoje/funcs.c
+++ hoje/funcs.c
@@ -82,12 +82,12 @@ printf("</script>\n");
 
 */
 void initAbstract(){r.frontmatter.abstract.paragraphs = g_array_new(FALSE,FALSE,sizeof(Paragraph_Elem));}
-void initKeywords(){r.frontmatter.abstract.keywords = g_array_new(FALSE,FALSE,sizeof(char*));}
+void initKeywords(){r.frontmatter.keywords = g_array_new(FALSE,FALSE,sizeof(char*));}
 void initAuthors(){r.frontmatter.authors = g_array_new(FALSE,FALSE,sizeof(Author));}
-void initAknowls(){r.frontmatter.abstract.abstract = g_array_new(FALSE,FALSE,sizeof(Paragraph));}
+void initAknowls(){r.frontmatter.aknow.paragraphs = g_array_new(FALSE,FALSE,sizeof(Paragraph));}
 void initBody(){r.body.chapters = g_array_new(FALSE,FALSE,sizeof(Chapter));}
 
-Paragraph initParagraph(Paragraph p){p.prg_elem= g_array_new(FALSE, FALSE, sizeof(P));return p;} 
+Paragraph initParagraph(Paragraph p){p.prg_elem= g_array_new(FALSE, FALSE, sizeof(Paragraph_Elem));return p;} 
 Chapter initChapter(Chapter c){ c.elems =g_array_new(FALSE, FALSE, sizeof(Elem));return c;}
 Table initTable(Table t){ t.rows =g_array_new(FALSE, FALSE, sizeof(Row)); t.size=0; return t;}
 
